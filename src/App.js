53a2c307961dc46b5f184ccb82c4095527c85add
initiateseven <initiateseven@gmail.com>
2019-05-26 15:23:47 +0200

    Evo malo nesto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {MDBContainer} from "mdbreact";
+import NavBar from './Navbar/Navbar.jsx';
+import Jumbo from './Jumbo/Jumbo.jsx'
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <MDBContainer>
+        <NavBar />
+        <Jumbo />
+      </MDBContainer>
+    );
+  }
 }
 
 export default App;
