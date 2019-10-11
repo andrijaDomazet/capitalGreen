a135fcfc42534a50b10ca1a909136cc410eef003
andrijaDomazet <domazet.andrija@gmail.com>
2019-10-11 02:41:29 +0200

    Activated backdrop

--- src/components/Banner/Banner2.js
+++ src/components/Banner/Banner2.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import "./Banner2.scss";
+
 export default class Banner2 extends Component {
   render() {
+    let classes = this.props.small ? "banner2Div small" : "banner2Div";
     return (
-      <div className="banner2Div">
+      <div className={classes}>
         <p>
           <span>Capital green</span>
           Vaš partner na putu ka modernoj poljoprivrednoj proizvodnji!
--- src/components/News/News.js
+++ src/components/News/News.js
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 import "./News.scss";
 export default class News extends Component {
   render() {
+    let classes = this.props.small ? "newsDiv small" : "newsDiv";
     return (
-      <div className="newsDiv">
+      <div className={classes}>
         <p>
           <span>News1</span>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem odio
