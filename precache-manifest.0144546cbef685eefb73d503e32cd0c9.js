fbfe6c23f10500cdc1924e913dfadee199159f5e
andrijaDomazet <domazet.andrija@gmail.com>
2020-03-22 00:28:21 +0100

    Updates

---
+++ precache-manifest.0144546cbef685eefb73d503e32cd0c9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb6eb875dc43ea39c0662bc6b7d90610",
+    "url": "/capitalGreen/index.html"
+  },
+  {
+    "revision": "e733ea1f3ab601dcb037",
+    "url": "/capitalGreen/static/css/2.f93178fb.chunk.css"
+  },
+  {
+    "revision": "ca8d066aa7c1164ae426",
+    "url": "/capitalGreen/static/css/main.21806ebb.chunk.css"
+  },
+  {
+    "revision": "e733ea1f3ab601dcb037",
+    "url": "/capitalGreen/static/js/2.28835531.chunk.js"
+  },
+  {
+    "revision": "f88b36470b8b650da4a18244e35dd001",
+    "url": "/capitalGreen/static/js/2.28835531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca8d066aa7c1164ae426",
+    "url": "/capitalGreen/static/js/main.233cd147.chunk.js"
+  },
+  {
+    "revision": "3ff168b1fe999cfe417a",
+    "url": "/capitalGreen/static/js/runtime-main.e7aa32d7.js"
+  },
+  {
+    "revision": "77531b6553e5279b93638ab15960acd8",
+    "url": "/capitalGreen/static/media/background.77531b65.jpg"
+  },
+  {
+    "revision": "7b25bdd1f176126618d184db340894b0",
+    "url": "/capitalGreen/static/media/djubrivo.7b25bdd1.jpg"
+  },
+  {
+    "revision": "6673bf88f38a392c6f4e73dc87042327",
+    "url": "/capitalGreen/static/media/logo.6673bf88.png"
+  },
+  {
+    "revision": "628fdb61584050a31a76c5137cd52369",
+    "url": "/capitalGreen/static/media/pozadina6.628fdb61.jpg"
+  },
+  {
+    "revision": "3c10112c6c4dfe8deb1783a04f63a053",
+    "url": "/capitalGreen/static/media/prvaStrana.3c10112c.jpg"
+  },
+  {
+    "revision": "34504d433e3446df425ac82c2b98737f",
+    "url": "/capitalGreen/static/media/slika1.34504d43.jpg"
+  },
+  {
+    "revision": "18dce33dd299654d043ef95b746756a7",
+    "url": "/capitalGreen/static/media/slika2.18dce33d.jpg"
+  },
+  {
+    "revision": "ad6138c010fc48bca907d6b099d61008",
+    "url": "/capitalGreen/static/media/slika3.ad6138c0.jpg"
+  }
+]);
