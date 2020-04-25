e44547e96761a5fced7ed9d30c044754169ee008
andrijaDomazet <domazet.andrija@gmail.com>
2020-04-25 22:19:40 +0200

    Import popup for products from list

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,39 +1,27 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import "./Popup.scss";
 import Slika from "./img/prvaStrana.jpg";
 import Buttons from "../../UI/Button/Buttons";
 
-const Naslov = props => {
+export default function Popup(props) {
+  const { show, details } = props;
+  let infoClasses = show ? "action__product open" : "action__product";
   return (
-    <div>
-      <h2>{props.children}</h2>
-    </div>
-  );
-};
-
-export default class Popup extends PureComponent {
-  state = {
-    val: "ORGANIC MINERAL Ca"
-  };
-  click = () => {
-    console.log("idemo");
-  };
-  render() {
-    const { click2, show } = this.props;
-    let infoClasses = show ? "action__product open" : "action__product";
-
-    return (
-      <div className={infoClasses}>
-        <img src={Slika} alt="picture" />
-        <div className="info__text">
-          <h1>AKCIJSKA PONUDA !!!</h1>
-          {<Naslov>{this.state.val}</Naslov>}
-          <Buttons clicked={click2}>Saznaj više...</Buttons>
-          <div className="info-close" onClick={this.props.removeCommentBox}>
-            <i className="fas fa-times" />
-          </div>
+    <div className={infoClasses}>
+      <img src={Slika} alt="picture" />
+      <div className="info__text">
+        <h1>AKCIJSKA PONUDA !!!</h1>
+        <h2>{details.name}</h2>
+        <ol>
+          <li>{details.pakovanje}</li>
+          <li>{details.price}</li>
+        </ol>
+        <p>{details.description}</p>
+        <Buttons>Saznaj više...</Buttons>
+        <div className="info-close" onClick={props.removeCommentBox}>
+          <i className="fas fa-times" />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
