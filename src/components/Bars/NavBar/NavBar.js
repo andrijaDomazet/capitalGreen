1bffcb82157840795afa412a7d746cfdc2d5801c
andrijaDomazet <domazet.andrija@gmail.com>
2020-04-27 22:42:15 +0200

    Organize files and shared data

--- src/components/Bars/NavBar/NavBar.js
+++ src/components/Bars/NavBar/NavBar.js
@@ -1,32 +1,26 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./NavBar.scss";
+import { options } from "../../../shared/routing";
 
 export default function NavBar(props) {
-  const navBarOptions = () => {
-    return (
-      <div className="links">
-        {options.map((option, index) => {
-          return (
-            <NavLink to={option.route} exact key={index} className="nav-link">
-              {option.title}
-            </NavLink>
-          );
-        })}
-        {adminFunction()}
-      </div>
-    );
-  };
-  const adminFunction = () => {
-    return props.admin ? <NavLink to="admin">Admin</NavLink> : null;
-  };
-  return <div className="navBar-div">{navBarOptions()}</div>;
+  return <div className="navBar-div">{navBarOptions(props)}</div>;
 }
-const options = [
-  { route: "/", title: "Naslovna" },
-  { route: "/o-nama", title: "O nama" },
-  { route: "/delatnosti", title: "Delatnosti" },
-  { route: "/proizvodi", title: "Proizvodi" },
-  { route: "/zaposlenje", title: "Zaposlenje" },
-  { route: "/kontakt", title: "Kontakt" }
-];
+
+const navBarOptions = (props) => {
+  return (
+    <div className="links">
+      {options.map((option, index) => {
+        return (
+          <NavLink to={option.route} exact key={index} className="nav-link">
+            {option.title}
+          </NavLink>
+        );
+      })}
+      {adminFunction(props)}
+    </div>
+  );
+};
+const adminFunction = (props) => {
+  return props.admin ? <NavLink to="admin">Admin</NavLink> : null;
+};
