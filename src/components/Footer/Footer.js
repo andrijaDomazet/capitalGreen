d5d2d21a57975cc0ef57d33148afba2af11b5549
andrijaDomazet <domazet.andrija@gmail.com>
2020-09-02 22:19:52 +0200

    Working on footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,28 @@
 import React from "react";
 import "./Footer.scss";
+import { options } from "../../shared/routing";
+import { NavLink } from "react-router-dom";
+import logo from "../../img/logo.png";
 export default function Footer() {
   return (
     <div className="footer">
       <div className="footer__top">
-        <div>logo</div>
-        <div>Brzi linkovi</div>
+        <div className="footer__logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="footer__links">
+          {options.map((option, index) => {
+            return (
+              <NavLink className="links" key={index} to={option.route}>
+                {option.title}
+              </NavLink>
+            );
+          })}
+        </div>
         <div>Partneri</div>
       </div>
       <div className="footer__bottom">
-        Created by <span>andrijaDomazet @2020</span>
+        Created by <span>Domy</span>
       </div>
     </div>
   );
