8ead91ef139167a1206d09756ce33e14e6c73eef
andrijaDomazet <domazet.andrija@gmail.com>
2020-09-04 00:12:46 +0200

    Updates

---
+++ precache-manifest.44ba0c2f7885d845e44b14c977c5bdd3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8ed936fc29cd5c0d55c300227a1d0ff",
+    "url": "/capitalGreen/index.html"
+  },
+  {
+    "revision": "9983661498d39590d03a",
+    "url": "/capitalGreen/static/css/main.f7fba7c0.chunk.css"
+  },
+  {
+    "revision": "0b1e6963d87515ed585f",
+    "url": "/capitalGreen/static/js/2.7a814154.chunk.js"
+  },
+  {
+    "revision": "dbe5ca7a82c728776bb536c3d2d64135",
+    "url": "/capitalGreen/static/js/2.7a814154.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9983661498d39590d03a",
+    "url": "/capitalGreen/static/js/main.d27a2b5c.chunk.js"
+  },
+  {
+    "revision": "c7d3943dd367b7a4854b",
+    "url": "/capitalGreen/static/js/runtime-main.29eb1f8f.js"
+  },
+  {
+    "revision": "7ce405c7aab27c4fcd3784ce00084980",
+    "url": "/capitalGreen/static/media/background.7ce405c7.jpg"
+  },
+  {
+    "revision": "3cab90946fcd79af5369d2d478fb2c5a",
+    "url": "/capitalGreen/static/media/djubrivo.3cab9094.jpg"
+  },
+  {
+    "revision": "aa183e8f3cf9df1095061312783ddddb",
+    "url": "/capitalGreen/static/media/logo.aa183e8f.png"
+  },
+  {
+    "revision": "95d0368e011a121c465fec35e34a4cf0",
+    "url": "/capitalGreen/static/media/pozadina1.95d0368e.jpeg"
+  },
+  {
+    "revision": "42a8a9a44fa6d9b930325e58b814c994",
+    "url": "/capitalGreen/static/media/pozadina8.42a8a9a4.jpeg"
+  },
+  {
+    "revision": "4ded776ed9251e312a1f6194484b49e5",
+    "url": "/capitalGreen/static/media/prvaStrana.4ded776e.jpg"
+  }
+]);
