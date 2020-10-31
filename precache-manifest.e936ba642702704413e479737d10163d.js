e6c105d208e1ee074a001fe1ab0fcde9ae159adb
andrijaDomazet <domazet.andrija@gmail.com>
2020-10-31 19:07:42 +0100

    Updates

---
+++ precache-manifest.e936ba642702704413e479737d10163d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ccf39b7924fc3a0ae8d902258817115",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c10e76841124039d557b",
+    "url": "/static/css/main.eecb5acc.chunk.css"
+  },
+  {
+    "revision": "a4c20f62226f6a777750",
+    "url": "/static/js/2.fc76bc68.chunk.js"
+  },
+  {
+    "revision": "dbe5ca7a82c728776bb536c3d2d64135",
+    "url": "/static/js/2.fc76bc68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c10e76841124039d557b",
+    "url": "/static/js/main.2e8d8c4b.chunk.js"
+  },
+  {
+    "revision": "d1380aa955913c90bfef",
+    "url": "/static/js/runtime-main.3eff5591.js"
+  },
+  {
+    "revision": "41d6db09712d0e1ab7b1202b57ce826e",
+    "url": "/static/media/drugaStrana.41d6db09.jpg"
+  },
+  {
+    "revision": "abe90ea92cb1f22e14d3ab1e93995ffc",
+    "url": "/static/media/folija.abe90ea9.jpg"
+  },
+  {
+    "revision": "aa183e8f3cf9df1095061312783ddddb",
+    "url": "/static/media/logo.aa183e8f.png"
+  },
+  {
+    "revision": "42a8a9a44fa6d9b930325e58b814c994",
+    "url": "/static/media/pozadina8.42a8a9a4.jpeg"
+  },
+  {
+    "revision": "4ded776ed9251e312a1f6194484b49e5",
+    "url": "/static/media/prvaStrana.4ded776e.jpg"
+  },
+  {
+    "revision": "1f2993f877e51963e430e92451d08aef",
+    "url": "/static/media/slika1.1f2993f8.jpg"
+  },
+  {
+    "revision": "c3e4b233c3cedd340c155af24c8ed03c",
+    "url": "/static/media/slika2.c3e4b233.jpg"
+  },
+  {
+    "revision": "f1378eea7f7990407d69ee53baae1d18",
+    "url": "/static/media/slika3.f1378eea.jpg"
+  },
+  {
+    "revision": "46c63541b820d54489c4188c79d82e82",
+    "url": "/static/media/zastita.46c63541.jpg"
+  }
+]);
