e955ccd6e23b479026f3625f14a378b09ad3e160
andrijaDomazet <domazet.andrija@gmail.com>
2020-11-17 23:32:07 +0100

    Change topBar redirect, clean code

--- src/components/Bars/TopBar/TopBar.js
+++ src/components/Bars/TopBar/TopBar.js
@@ -1,23 +1,20 @@
 import React from "react";
 import "./TopBar.scss";
+import { NavLink } from "react-router-dom";
 
 export default class TopBar extends React.Component {
-  optionsTopBar = () => {
-    return options.map((option, index) => {
-      return (
-        <a href={`tel:${option.title}`} className="topBar__link" key={index}>
-          <i className={`fas fa-${option.icon}`} />
-          {option.title}
-        </a>
-      );
-    });
-  };
   render() {
-    return <div className="topBar">{this.optionsTopBar()}</div>;
+    return (
+      <div className="topBar">
+        <NavLink to="/kontakt" exact className="topBar__link">
+          office@capitalgreen.rs
+        </NavLink>
+        <a href="tel:+381 64 5 222 333" className="topBar__link">
+          <i className="fas fa-phone-square" />
+          +381 64 5 222 333
+        </a>
+        <div className="topBar__link">Pon - Pet : 8:00 - 17:00</div>
+      </div>
+    );
   }
 }
-const options = [
-  { route: "#", title: "office@capitalgreen.rs", icon: "envelope" },
-  { route: "#", title: "+381 64 5 222 333", icon: "phone-square" },
-  { route: "#", title: "Pon - Pet : 8:00 - 17:00", icon: "clock" },
-];
