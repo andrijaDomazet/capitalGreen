c7be4b91b3421e10331817fb62f89a1b6417e7d7
andrijaDomazet <domazet.andrija@gmail.com>
2020-12-26 01:28:29 +0100

    Test sitemap

---
+++ src/sitemap-routes.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Route } from "react-router";
+
+export default (
+  <Route>
+    <Route path="/" />
+    <Route path="/bioloska-zastita" />
+    <Route path="/nas-tim" />
+    <Route path="/kontakt" />
+  </Route>
+);
